ff6f352b4845a010cf01815e9c455eb69b58958b
Brandt Campbell <bcampbell@talentpath.com>
2020-11-19 15:06:08 -0500

    fixed testing

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -3,34 +3,75 @@ package com.github.perschola;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+
+        String toReturn = "";
+        for(int i = start; i < stop; i++){
+            if(i%2 == 1){
+                toReturn += i;
+            }
+        }
+        return toReturn;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+
+        String toReturn = "";
+        for(int i = start; i < stop; i++){
+            if(i%2 == 0){
+                toReturn += i;
+            }
+        }
+        return toReturn;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+
+        String toReturn = "";
+        for(int i = start; i < stop; i = i + step){
+            toReturn += (int)Math.pow(i,2);
+        }
+        return toReturn;
     }
 
     public static String getRange(int start) {
-        return null;
+
+        String toReturn = "";
+        for(int i = 0; i < start; i++){
+            toReturn += i;
+        }
+        return toReturn;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+
+        String toReturn = "";
+        for(int i = start; i < stop; i++){
+            toReturn += i;
+        }
+        return toReturn;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+
+        String toReturn = "";
+        for(int i = start; i < stop; i = i + step){
+            toReturn += i;
+        }
+        return toReturn;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+
+        String toReturn = "";
+        for(int i = start; i < stop; i = i + step){
+            Double resultAsDouble = Math.pow(i,exponent);
+            Integer resultAsInteger = resultAsDouble.intValue();
+            toReturn += resultAsInteger;
+        }
+        return toReturn;
     }
 }
--- src/main/java/com/github/perschola/TriangleUtilities.java
+++ src/main/java/com/github/perschola/TriangleUtilities.java
@@ -3,18 +3,31 @@ package com.github.perschola;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < numberOfRows; i++) {
+             sb.append(getRow(i) + "\n");
+        }
+        return sb.toString();
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String star = "*";
+        String string = "";
+        for (int i = 0; i < numberOfStars; i++) {
+             string += star;
+        }
+        return string;
+
+//        return new String(new char[numberOfStars-1]).replaceAll("", "*");
     }
 
     public static String getSmallTriangle() {
-        return null;
+
+        return getTriangle(5);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(10);
     }
 }
